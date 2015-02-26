e87307287b04d281d27389eb3b280ea834601ea3
Эдуард <emustakimow@yandex.ru>
2015-02-27 00:26:40 +0300

    параметризовал вход (логин, пароль, смс код)

--- Logintest.cs
+++ Logintest.cs
@@ -29,31 +29,32 @@ namespace Modul
 
             ImplicitlyWait();
             //не забыть бы что здесь неявное ожидание в тесте
-            Login();
+            Login("9274450647","Maxpass5", "1111");
 
-            ImplicitlyWait();
-            //блок "неявное ожидание" надо добавлять перед каждым блоком, а то срабатывает только перед одним
+            //ImplicitlyWait();
+            //блок "неявное ожидание" надо добавлять перед каждым блоком, а то срабатывает только перед одним, 
+            //или нет, хз, надо будет уточнить
 
             LoginOut();
          }
 
         private void ImplicitlyWait()
         {
-            wd.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+            wd.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
         }
-       
-        private void Login()
+
+        private void Login(string phone, string password, string smscode)
         {
         wd.FindElement(By.Name("phone")).Click();
         wd.FindElement(By.Name("phone")).Clear();
-        wd.FindElement(By.Name("phone")).SendKeys("+7 927-445-06-47");
+        wd.FindElement(By.Name("phone")).SendKeys(phone);
         wd.FindElement(By.Name("password")).Click();
         wd.FindElement(By.Name("password")).Clear();
-        wd.FindElement(By.Name("password")).SendKeys("Maxpass5");
+        wd.FindElement(By.Name("password")).SendKeys(password);
         wd.FindElement(By.XPath("//div[@class='auth-bl']//button[.='Войти']")).Click();
         wd.FindElement(By.XPath("//div[@class='sms-bl__inner']/div/input")).Click();
         wd.FindElement(By.XPath("//div[@class='sms-bl__inner']/div/input")).Clear();
-        wd.FindElement(By.XPath("//div[@class='sms-bl__inner']/div/input")).SendKeys("1111");
+        wd.FindElement(By.XPath("//div[@class='sms-bl__inner']/div/input")).SendKeys(smscode);
         wd.FindElement(By.XPath("//div[@class='sms-bl__inner']//button[.='Подтвердить']")).Click();
         }
        
