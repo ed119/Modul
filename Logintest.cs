2e5efac931a737281338094dcc4c012cb96138a5
Эдуард <emustakimow@yandex.ru>
2015-02-26 23:40:35 +0300

    Разбил блоки логин, логаут и имплиситливейт

--- Logintest.cs
+++ Logintest.cs
@@ -18,7 +18,7 @@ namespace Modul
         {
             wd = new ChromeDriver();
             wd.Navigate().GoToUrl("https://low.modulbank.ru/login");
-            // включаем неявное ожидание на все FindElement
+            
             
         }
         
@@ -26,7 +26,24 @@ namespace Modul
             [Test()]
         public void LoginTest()
         {
-        wd.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+
+            ImplicitlyWait();
+            //не забыть бы что здесь неявное ожидание в тесте
+            Login();
+
+            ImplicitlyWait();
+            //блок "неявное ожидание" надо добавлять перед каждым блоком, а то срабатывает только перед одним
+
+            LoginOut();
+         }
+
+        private void ImplicitlyWait()
+        {
+            wd.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+        }
+       
+        private void Login()
+        {
         wd.FindElement(By.Name("phone")).Click();
         wd.FindElement(By.Name("phone")).Clear();
         wd.FindElement(By.Name("phone")).SendKeys("+7 927-445-06-47");
@@ -38,8 +55,14 @@ namespace Modul
         wd.FindElement(By.XPath("//div[@class='sms-bl__inner']/div/input")).Clear();
         wd.FindElement(By.XPath("//div[@class='sms-bl__inner']/div/input")).SendKeys("1111");
         wd.FindElement(By.XPath("//div[@class='sms-bl__inner']//button[.='Подтвердить']")).Click();
-        wd.FindElement(By.CssSelector("a.b-leftmenu__exit")).Click();
-            }
+        }
+       
+        private void LoginOut()
+        {
+            wd.FindElement(By.CssSelector("a.b-leftmenu__exit")).Click();
+        }
+       
+            
         [TearDown]
         public void StopBrowser()
         {
