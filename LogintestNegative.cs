56f9b3be437c447075a8ff59f3ce5e44ffc63db1
Эдуард <emustakimow@yandex.ru>
2015-02-27 04:17:08 +0300

    Запуск браузера для всех тестов, работает

--- LogintestNegative.cs
+++ LogintestNegative.cs
@@ -9,18 +9,9 @@ namespace Modul
 {
 
     [TestFixture()]
-    public class LoginTestsNegative
+    public class LoginTestsNegative : Base
     {
-        private IWebDriver wd;
-
-        [SetUp]
-        public void StartBrowser()
-        {
-            wd = new ChromeDriver();
-            wd.Navigate().GoToUrl("https://low.modulbank.ru/login");
-
-
-        }
+      
 
 
         [Test()]
@@ -29,63 +20,11 @@ namespace Modul
 
             ImplicitlyWait();
             //не забыть бы что здесь неявное ожидание в тесте
-            Login("9274450648", "Maxpass5");
+            LoginWithoutSms("9274450648", "Maxpass5");
             Assert.IsTrue(IsNotLoginIn(), "Not logged in");
             //раз не лендинг не попадаем, то проверка по полю "телефон"
         }
 
-        private void ImplicitlyWait()
-        {
-            wd.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
-        }
-
-        private void Login(string phone, string password)
-        {
-            wd.FindElement(By.Name("phone")).Click();
-            wd.FindElement(By.Name("phone")).Clear();
-            wd.FindElement(By.Name("phone")).SendKeys(phone);
-            wd.FindElement(By.Name("password")).Click();
-            wd.FindElement(By.Name("password")).Clear();
-            wd.FindElement(By.Name("password")).SendKeys(password);
-            wd.FindElement(By.XPath("//div[@class='auth-bl']//button[.='Войти']")).Click();
-           
-        }
-
-        private bool IsNotLoginIn()
-        {
-            return IsElementPresent(By.Name("phone"));
-        }
-        private bool IsLoginIn()
-        {
-            return IsElementPresent(By.CssSelector("a.b-leftmenu__exit"));
-        }
-
-        private bool IsLoginOut()
-        {
-            return IsElementPresent(By.LinkText("Личный кабинет"));
-        }
-
-        private bool IsElementPresent(By by)
-        {
-            return wd.FindElements(by).Count > 0;
-
-        }
-
-        private void LoginOut()
-        {
-            wd.FindElement(By.CssSelector("a.b-leftmenu__exit")).Click();
-        }
-
-
-        [TearDown]
-        public void StopBrowser()
-        {
-            if (wd != null)
-            {
-                wd.Quit();
-                wd = null;
-            }
-
-        }
+    
     }
 }
--- LogintestPositive.cs
+++ LogintestPositive.cs
@@ -9,19 +9,9 @@ namespace Modul
 {
 
     [TestFixture()]
-    public class LoginTestsPositive
+    public class LoginTestsPositive : Base
     {
-        private IWebDriver wd;
-       
-        [SetUp]
-        public void StartBrowser()
-        {
-            wd = new ChromeDriver();
-            wd.Navigate().GoToUrl("https://low.modulbank.ru/login");
-            
-            
-        }
-        
+     
             
             [Test()]
         public void LoginTestWithValidCredentials()
@@ -29,64 +19,11 @@ namespace Modul
 
             ImplicitlyWait();
             //не забыть бы что здесь неявное ожидание в тесте
-            Login("9274450647","Maxpass5", "1111");
+            LoginWithSms("9274450647", "Maxpass5", "1111");
             Assert.IsTrue(IsLoginIn(), "Logged in");            
             LoginOut();
             Assert.IsTrue(IsLoginOut(), "Lending");
          }
 
-        private void ImplicitlyWait()
-        {
-            wd.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
-        }
-
-        private void Login(string phone, string password, string smscode)
-        {
-        wd.FindElement(By.Name("phone")).Click();
-        wd.FindElement(By.Name("phone")).Clear();
-        wd.FindElement(By.Name("phone")).SendKeys(phone);
-        wd.FindElement(By.Name("password")).Click();
-        wd.FindElement(By.Name("password")).Clear();
-        wd.FindElement(By.Name("password")).SendKeys(password);
-        wd.FindElement(By.XPath("//div[@class='auth-bl']//button[.='Войти']")).Click();
-        wd.FindElement(By.XPath("//div[@class='sms-bl__inner']/div/input")).Click();
-        wd.FindElement(By.XPath("//div[@class='sms-bl__inner']/div/input")).Clear();
-        wd.FindElement(By.XPath("//div[@class='sms-bl__inner']/div/input")).SendKeys(smscode);
-        wd.FindElement(By.XPath("//div[@class='sms-bl__inner']//button[.='Подтвердить']")).Click();
-        }
-
-        private bool IsLoginIn()
-        {
-            return IsElementPresent(By.CssSelector("a.b-leftmenu__exit"));
-        }
-
-        private bool IsLoginOut()
-        {
-            return IsElementPresent(By.LinkText("Личный кабинет"));
-        }
-
-        private bool IsElementPresent(By by)
-        {
-            return wd.FindElements(by).Count > 0;
-
-        }
-       
-        private void LoginOut()
-        {
-            wd.FindElement(By.CssSelector("a.b-leftmenu__exit")).Click();
-        }
-       
-            
-        [TearDown]
-        public void StopBrowser()
-        {
-            if (wd != null)
-
-            {
-                wd.Quit();
-                wd = null;
-            }
-      
-        }
     }
 }
