ccc0adf3fb8f1305cbd7a9ae7b6d6ffcda8c6808
Эдуард <emustakimow@yandex.ru>
2015-02-27 05:54:03 +0300

    Создал апликейшн менеджер с помошниками

---
+++ Impl/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul
+{
+    public class NavigationHelper
+    {
+        private ApplicationManager manager;
+
+        public NavigationHelper(ApplicationManager manager)
+        {
+            this.manager = manager;
+        }
+    }
+}
