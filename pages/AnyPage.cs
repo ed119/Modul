0d106b7b949dab65ff6fa973b9883d4452fb7264
Эдуард <emustakimow@yandex.ru>
2015-03-02 02:53:26 +0300

    Добавил Pages

---
+++ pages/AnyPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul
+{
+    public class AnyPage
+    {
+        private PageManager pageManager;
+
+        public AnyPage(PageManager pageManager)
+        {
+            this.pageManager = pageManager;
+        }
+    }
+}
---
+++ pages/PageManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Modul
+{
+    public class PageManager
+    {
+        private IWebDriver driver;
+
+        public PageManager(OpenQA.Selenium.IWebDriver Driver)
+        {
+            this.driver = Driver;
+            Login = InitElements(new LoginPage(this));
+            Internal = InitElements(new InternalPage(this));
+            UserProfile = InitElements(new UserProfilePage(this));
+        }
+
+        private T InitElements<T>(T page) where T : AnyPage
+        {
+            PageFactory.InitElements(driver, page);
+            return page;
+        }
+
+        public InternalPage Internal { get; set; }
+
+        public LoginPage Login { get; set; }
+
+        public UserProfilePage UserProfile { get; set; }
+    }
+}
